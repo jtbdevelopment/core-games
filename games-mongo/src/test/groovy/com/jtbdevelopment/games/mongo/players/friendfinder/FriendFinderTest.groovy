b8f11251c2aa279337ba66dccf1318d493daac67
JTB <jtbdevelopment@gmail.com>
2015-01-09 06:37:03 -0500

    mongo friend finder tests

--- games-mongo/src/test/groovy/com/jtbdevelopment/games/mongo/players/friendfinder/FriendFinderTest.groovy
+++ games-mongo/src/test/groovy/com/jtbdevelopment/games/mongo/players/friendfinder/FriendFinderTest.groovy
@@ -3,7 +3,6 @@ package com.jtbdevelopment.games.mongo.players.friendfinder
 import org.springframework.beans.factory.config.ConfigurableBeanFactory
 import org.springframework.context.annotation.Scope
 import org.springframework.context.annotation.ScopedProxyMode
-import org.springframework.stereotype.Component
 
 /**
  * Date: 1/8/15
@@ -13,11 +12,9 @@ class FriendFinderTest extends GroovyTestCase {
     FriendFinder finder = new FriendFinder()
 
     void testClassAnnotations() {
-        assert FriendFinder.class.getAnnotation(Component.class)
         Scope scope = FriendFinder.class.getAnnotation(Scope.class)
         assert scope
         assert scope.proxyMode() == ScopedProxyMode.INTERFACES
         assert scope.value() == ConfigurableBeanFactory.SCOPE_PROTOTYPE
-
     }
 }
