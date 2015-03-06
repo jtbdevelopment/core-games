fb41cfe16df55ae272dda5362463f6f6dc0b1feb
JTB <jtbdevelopment@gmail.com>
2015-03-05 20:49:28 -0500

    moved some hazelcast to core

--- games/src/main/groovy/com/jtbdevelopment/games/players/GameSpecificPlayerAttributes.groovy
+++ games/src/main/groovy/com/jtbdevelopment/games/players/GameSpecificPlayerAttributes.groovy
@@ -4,6 +4,6 @@ package com.jtbdevelopment.games.players
  * Date: 1/30/15
  * Time: 6:56 PM
  */
-interface GameSpecificPlayerAttributes {
+interface GameSpecificPlayerAttributes extends Serializable {
 
 }
