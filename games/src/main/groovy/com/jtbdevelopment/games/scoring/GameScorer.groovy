967f5c36243c3760c6905b6652d94d87896cc2cb
JTB <jtbdevelopment@gmail.com>
2015-03-28 14:07:24 -0400

    scorer interface

---
+++ games/src/main/groovy/com/jtbdevelopment/games/scoring/GameScorer.groovy
@@ -0,0 +1,13 @@
+package com.jtbdevelopment.games.scoring
+
+import com.jtbdevelopment.games.games.Game
+import groovy.transform.CompileStatic
+
+/**
+ * Date: 3/28/15
+ * Time: 2:03 PM
+ */
+@CompileStatic
+interface GameScorer<IMPL extends Game> {
+    public IMPL scoreGame(final IMPL game)
+}
