2928e855ce41d2b6efdacbd76a2df0f778eface5
JTB <jtbdevelopment@gmail.com>
2016-07-12 18:52:47 -0400

    fixed websocket under jetty/atmosphere

--- games-dev-utilities/src/main/groovy/com/jtbdevelopment/games/dev/utilities/jetty/JettyServer.groovy
+++ games-dev-utilities/src/main/groovy/com/jtbdevelopment/games/dev/utilities/jetty/JettyServer.groovy
@@ -69,7 +69,6 @@ class JettyServer {
         atmosphereServletHolder.setInitParameter("org.atmosphere.websocket.messageContentType", "application/json")
         atmosphereServletHolder.setInitParameter("org.atmosphere.cpr.broadcasterLifeCyclePolicy", "EMPTY_DESTROY")
         atmosphereServletHolder.setInitParameter("org.atmosphere.cpr.sessionSupport", "true")
-        atmosphereServletHolder.setInitParameter("org.atmosphere.websocket.suppressJSR356", "true")
         atmosphereServletHolder.setInitParameter("org.atmosphere.interceptor.HeartbeatInterceptor.heartbeatFrequencyInSeconds", "20")
         atmosphereServletHolder.setInitParameter("org.atmosphere.cpr.dropAccessControlAllowOriginHeader", "true")
         atmosphereServletHolder.asyncSupported = true
