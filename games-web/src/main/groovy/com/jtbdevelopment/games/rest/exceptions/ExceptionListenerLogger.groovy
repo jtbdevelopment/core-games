26842260e34e90c167decc95c371d30061b3dd1b
JTB <jtbdevelopment@gmail.com>
2016-10-02 09:17:23 -0400

    register a logger

---
+++ games-web/src/main/groovy/com/jtbdevelopment/games/rest/exceptions/ExceptionListenerLogger.groovy
@@ -0,0 +1,34 @@
+package com.jtbdevelopment.games.rest.exceptions
+
+import org.glassfish.jersey.server.monitoring.ApplicationEvent
+import org.glassfish.jersey.server.monitoring.ApplicationEventListener
+import org.glassfish.jersey.server.monitoring.RequestEvent
+import org.glassfish.jersey.server.monitoring.RequestEventListener
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import javax.ws.rs.ext.Provider
+
+/**
+ * Date: 10/2/16
+ * Time: 8:22 AM
+ *
+ * TODO - should this be in core common
+ */
+@Provider
+class ExceptionListenerLogger implements ApplicationEventListener, RequestEventListener {
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionListenerLogger.class)
+
+    void onEvent(final RequestEvent event) {
+        if (event.type == RequestEvent.Type.ON_EXCEPTION) {
+            logger.error("REST Exception", event.exception)
+        }
+    }
+
+    void onEvent(final ApplicationEvent event) {
+    }
+
+    RequestEventListener onRequest(final RequestEvent requestEvent) {
+        return this
+    }
+}
