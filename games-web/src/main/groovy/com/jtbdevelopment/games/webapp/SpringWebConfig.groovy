84e3a8b73d0a15ad33fcc77a45aa542244580e2e
JTB <jtbdevelopment@gmail.com>
2016-10-18 23:34:54 -0400

    no more web.xml

---
+++ games-web/src/main/groovy/com/jtbdevelopment/games/webapp/SpringWebConfig.groovy
@@ -0,0 +1,19 @@
+package com.jtbdevelopment.games.webapp
+
+import groovy.transform.CompileStatic
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.annotation.Order
+
+/**
+ * Date: 10/18/16
+ * Time: 8:07 PM
+ */
+@Configuration
+@ComponentScan(basePackages = "com.jtbdevelopment")
+@PropertySource(value = "classpath:twisted.properties", ignoreResourceNotFound = true)
+@CompileStatic
+@Order(value = -1)
+public class SpringWebConfig {
+}
