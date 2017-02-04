f1b4a909cb75326c7bde7b8b2f6876947b1eb971
JTB <jtbdevelopment@gmail.com>
2017-02-04 10:07:29 -0500

    added single player game services

---
+++ games-web-std-singleplayer/src/main/groovy/com/jtbdevelopment/games/rest/AbstractSinglePlayerGameServices.groovy
@@ -0,0 +1,15 @@
+package com.jtbdevelopment.games.rest
+
+import com.jtbdevelopment.games.rest.services.AbstractGameServices
+import groovy.transform.CompileStatic
+
+/**
+ * Date: 4/8/2015
+ * Time: 10:26 PM
+ */
+@SuppressWarnings("GroovyUnusedDeclaration")
+@CompileStatic
+abstract class AbstractSinglePlayerGameServices<ID extends Serializable> extends AbstractGameServices<ID> {
+    //  Currently nothing
+
+}
