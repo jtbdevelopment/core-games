a4e33652bf0d0bb89781f2ca25b2e15e3193785c
JTB <jtbdevelopment@gmail.com>
2018-04-19 21:59:29 -0400

    de-groovying

---
+++ games-web/src/main/java/com/jtbdevelopment/games/security/spring/social/facebook/FacebookFriendFinder.java
@@ -0,0 +1,65 @@
+package com.jtbdevelopment.games.security.spring.social.facebook;
+
+import com.jtbdevelopment.games.dao.AbstractPlayerRepository;
+import com.jtbdevelopment.games.players.Player;
+import com.jtbdevelopment.games.players.friendfinder.SourceBasedFriendFinder;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Reference;
+import org.springframework.stereotype.Component;
+
+/**
+ * Date: 12/20/2014 Time: 11:17 PM
+ */
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.INTERFACES)
+public class FacebookFriendFinder implements SourceBasedFriendFinder {
+
+  @Autowired
+  protected AbstractPlayerRepository playerRepository;
+  @Autowired(required = false)
+  protected Facebook facebook;
+
+  @Override
+  public boolean handlesSource(final String source) {
+    return "facebook".equals(source) && facebook != null;
+  }
+
+  @Override
+  public Map<String, Set<?>> findFriends(final Player player) {
+    Map<String, Set<Object>> results = new HashMap<>();
+    results.put(FRIENDS_KEY, new LinkedHashSet<>());
+    results.put(NOT_FOUND_KEY, new LinkedHashSet<>());
+    results.put(INVITABLE_FRIENDS_KEY, new LinkedHashSet<>());
+
+    PagedList<Reference> friends = facebook.friendOperations().getFriends();
+    List<String> friendSourceIds = friends.stream().map(f -> f.getId())
+        .collect(Collectors.toList());
+    List<Player<?>> players = playerRepository
+        .findBySourceAndSourceIdIn("facebook", friendSourceIds);
+    Map<String, Player<?>> sourceIdMap = players.stream()
+        .collect(Collectors.toMap(p -> p.getSourceId(), p -> p));
+    friends.forEach(friendReference -> {
+      Player friend = sourceIdMap.get(friendReference.getId());
+      if (friend != null) {
+        results.get(FRIENDS_KEY).add(friend);
+      } else {
+        results.get(NOT_FOUND_KEY).add(friendReference);
+      }
+    });
+    PagedList<Reference> canInvite = facebook
+        .fetchConnections("me", "invitable_friends", Reference.class);
+    results.get(INVITABLE_FRIENDS_KEY).addAll(canInvite);
+    return new HashMap<>(results);
+  }
+}
