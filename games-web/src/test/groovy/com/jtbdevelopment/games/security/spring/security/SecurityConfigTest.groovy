15ce211415312d0c3f17f54ae334822b71e46f7c
JTB <jtbdevelopment@gmail.com>
2018-04-29 19:21:25 -0400

    de-groovying

--- games-web/src/test/groovy/com/jtbdevelopment/games/security/spring/security/SecurityConfigTest.groovy
+++ games-web/src/test/groovy/com/jtbdevelopment/games/security/spring/security/SecurityConfigTest.groovy
@@ -2,67 +2,74 @@ package com.jtbdevelopment.games.security.spring.security
 
 import com.jtbdevelopment.core.spring.security.crypto.password.InjectedBCryptPasswordEncoder
 import com.jtbdevelopment.games.security.spring.userdetails.PlayerUserDetailsService
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
-import org.springframework.security.config.annotation.ObjectPostProcessor
+import org.springframework.security.authentication.encoding.PasswordEncoder
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository
+import org.springframework.test.util.ReflectionTestUtils
 
 import javax.annotation.PostConstruct
 import java.lang.reflect.Method
+
+import static org.junit.Assert.*
+import static org.mockito.Matchers.isA
 
 /**
  * Date: 1/12/15
  * Time: 7:01 PM
  */
-class SecurityConfigTest extends GroovyTestCase {
-    SecurityConfig securityConfig = new SecurityConfig()
+class SecurityConfigTest {
+    private SecurityConfig securityConfig = new SecurityConfig()
 
+    @Test
     void testClassAnnotations() {
-        assert SecurityConfig.class.isAnnotationPresent(Configuration.class)
-        assert SecurityConfig.class.isAnnotationPresent(EnableWebSecurity.class)
-        assert SecurityConfig.class.isAnnotationPresent(EnableGlobalMethodSecurity.class)
-        assert SecurityConfig.class.getAnnotation(EnableGlobalMethodSecurity.class).jsr250Enabled()
+        assertTrue SecurityConfig.class.isAnnotationPresent(Configuration.class)
+        assertTrue SecurityConfig.class.isAnnotationPresent(EnableWebSecurity.class)
+        assertTrue SecurityConfig.class.isAnnotationPresent(EnableGlobalMethodSecurity.class)
+        assertTrue SecurityConfig.class.getAnnotation(EnableGlobalMethodSecurity.class).jsr250Enabled()
     }
 
+    @Test
     void testSetsUpAuthenticationDatabase() {
         Method m = SecurityConfig.class.getMethod('configureAuthenticationProvider')
-        assert m
-        assert m.isAnnotationPresent(PostConstruct.class)
+        assertNotNull m
+        assertTrue m.isAnnotationPresent(PostConstruct.class)
 
-        AuthenticationProvider provider
-        boolean encodeCalled = false
-        securityConfig.injectedPasswordEncoder = [
-                encode: {
-                    String pass ->
-                        encodeCalled = true
-                        return pass;
-                }
-        ] as InjectedBCryptPasswordEncoder
-        securityConfig.playerUserDetailsService = [] as PlayerUserDetailsService
-        securityConfig.authenticationManagerBuilder = new AuthenticationManagerBuilder([] as ObjectPostProcessor) {
+        InjectedBCryptPasswordEncoder passwordEncoder = Mockito.mock(InjectedBCryptPasswordEncoder.class)
+        Mockito.when(passwordEncoder.encode("userNotFoundPassword")).thenReturn("XYZ")
+        Mockito.when(passwordEncoder.encode("test")).thenReturn("pass")
+        securityConfig.injectedPasswordEncoder = passwordEncoder
+        PlayerUserDetailsService userDetailsService = Mockito.mock(PlayerUserDetailsService.class)
+        securityConfig.playerUserDetailsService = userDetailsService
+
+        AuthenticationManagerBuilder managerBuilder = Mockito.mock(AuthenticationManagerBuilder.class)
+        Mockito.when(managerBuilder.authenticationProvider(isA(DaoAuthenticationProvider.class))).then(new Answer<Object>() {
             @Override
-            AuthenticationManagerBuilder authenticationProvider(
-                    final AuthenticationProvider authenticationProvider) {
-                provider = authenticationProvider
-                return super.authenticationProvider(authenticationProvider)
+            Object answer(InvocationOnMock invocation) throws Throwable {
+                DaoAuthenticationProvider daoAuthenticationProvider = invocation.arguments[0]
+                def encoder = (PasswordEncoder) ReflectionTestUtils.getField(daoAuthenticationProvider, "passwordEncoder");
+                assertEquals("pass", encoder.encodePassword("test", null));
+                assertSame(userDetailsService, ReflectionTestUtils.getField(daoAuthenticationProvider, "userDetailsService"));
+                return managerBuilder;
             }
+        })
+        securityConfig.authenticationManagerBuilder = managerBuilder
 
-        }
         securityConfig.configureAuthenticationProvider()
-        assert provider
-        assert provider instanceof DaoAuthenticationProvider
-        //  Not possible to directly validate
-        //assert provider.passwordEncoder.is(securityConfig.injectedPasswordEncoder)
-        assert encodeCalled
-        assert provider.userDetailsService.is(securityConfig.playerUserDetailsService)
+
+        Mockito.verify(managerBuilder).authenticationProvider(isA(DaoAuthenticationProvider.class))
     }
 
+    @Test
     void testCsrfRepositoryTokenHeader() {
         HttpSessionCsrfTokenRepository repository = securityConfig.csrfTokenRepository()
-        assert repository.headerName == 'X-XSRF-TOKEN'
+        assertEquals 'X-XSRF-TOKEN', repository.headerName
     }
 }
