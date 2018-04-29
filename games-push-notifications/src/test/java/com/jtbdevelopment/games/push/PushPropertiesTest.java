ac799e805c7b7681d84b5454823a3e141c116ee8
JTB <jtbdevelopment@gmail.com>
2018-04-29 18:28:37 -0400

    de-groovying

--- games-push-notifications/src/test/java/com/jtbdevelopment/games/push/PushPropertiesTest.java
+++ games-push-notifications/src/test/java/com/jtbdevelopment/games/push/PushPropertiesTest.java
@@ -1,12 +1,29 @@
 package com.jtbdevelopment.games.push;
 
+import static org.junit.Assert.assertEquals;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * Date: 10/16/15 Time: 6:59 PM
  */
 public class PushPropertiesTest {
+
+  @Test
+  public void testConstructorParamAnnotations() throws NoSuchMethodException {
+    Constructor c = PushProperties.class
+        .getConstructor(String.class, String.class);
+    Annotation[][] annotations = c.getParameterAnnotations();
+    assertEquals(2, annotations.length);
+    assertEquals(Value.class, annotations[0][0].annotationType());
+    assertEquals(Value.class, annotations[1][0].annotationType());
+    assertEquals("${push.senderID:}", ((Value) annotations[0][0]).value());
+    assertEquals("${push.apiKey:}", ((Value) annotations[1][0]).value());
+  }
 
   @Test
   public void testEnabledFlagSetWithBothValuesSet() {
